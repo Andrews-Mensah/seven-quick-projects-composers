7be84b36cfae3cb29e3256894a6c327b5f685586
Andrews-Mensah <82477892+Andrews-Mensah@users.noreply.github.com>
2021-04-21 13:46:22 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = angular.module("ComposerList",[]);
+
+app.controller("ComposerController", function ($scope) {
+    
+    $scope.pieces = [
+        {"pic": "c1.png", "composer":"Bach", "title":"Brandenburg Concertos"},
+        {"pic": "c1.png", "composer":"Bach", "title":"Christmas Oratorio"},
+        {"pic": "c1.png", "composer":"Bach", "title":"Goldberg Variations"},
+        {"pic": "c1.png", "composer":"Bach", "title":"Ave Maria"},
+        {"pic": "c1.png", "composer":"Bach", "title":"St. Matthew Passion"},
+        {"pic": "c2.png", "composer":"Mozart", "title":"The Magic Flute"},
+        {"pic": "c2.png", "composer":"Mozart", "title":"Requiem"},
+        {"pic": "c2.png", "composer":"Mozart", "title":"The Marriage of Figaro"},
+        {"pic": "c2.png", "composer":"Mozart", "title":"Eine kleine Nachtmusik"},
+        {"pic": "c2.png", "composer":"Mozart", "title":"Clarinet Concerto"},
+        {"pic": "c3.png", "composer":"Beethoven", "title":"Für Elise"},
+        {"pic": "c3.png", "composer":"Beethoven", "title":"Violin Concerto"},
+        {"pic": "c3.png", "composer":"Beethoven", "title":"Fidelio"},
+        {"pic": "c3.png", "composer":"Beethoven", "title":"Anthem of Europe"},
+        {"pic": "c3.png", "composer":"Beethoven", "title":"Turkish March"},
+        
+
+
+
+
+    ]
+})
